9bb36fbbcc773d26a5c0a9a2cc878aa1da0f9f6a
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-04-30 15:52:23 +0530

    added volume levels

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let audioContext, analyzer, source, dataArray, bufferLength, isRunning = false;
 let count = 1;
+
 function startNoiseDetection() {
   audioContext = new AudioContext();
 
@@ -32,17 +33,36 @@ function detectNoise() {
 
   requestAnimationFrame(detectNoise);
   analyzer.getByteFrequencyData(dataArray);
+  console.log(dataArray);
   const average = dataArray.reduce((a, b) => a + b) / bufferLength;
   const frequency = (average / 255) * audioContext.sampleRate / 2;
-  // document.getElementById("frequency").textContent = frequency.toFixed(2) + " Hz";
-  const idname = `frequency${count}`
-  // console.log(idname);
-  document.getElementById(idname).style.height = frequency/80 + 'px' ;
-  // console.log(document.getElementById(idname).style.width);
-  count = count +1 ;
+  const volume = average / 255;
+
+  // Add volume recognition based on the volume value
+  let volumeLabel;
+  if (volume < 0.2) {
+    volumeLabel = "Low Volume";
+  } else if (volume < 0.35) {
+    volumeLabel = "Medium Volume";
+  } else {
+    volumeLabel = "High Volume";
+  }
+
+  // Calculate decibel values using a reference level
+  const referenceLevel = -50; // Adjust this value based on your microphone sensitivity and environment
+  const decibels = 20 * Math.log10(frequency) - referenceLevel;
+
+  // Update the HTML elements with the volume label, decibel value, and frequency
+  document.getElementById("volume-label").textContent = "Volume: " + volumeLabel;
+  document.getElementById("decibel-value").textContent = "Decibels: " + decibels.toFixed(2) + " dB";
+  document.getElementById("frequency-value").textContent = "Frequency: " + frequency.toFixed(2) + " Hz";
+
+  const idname = `frequency${count}`;
+  document.getElementById(idname).style.height = volume * 200 + 'px';
+
+  count = count + 1;
   const storingbox = document.getElementById('graph').innerHTML;
-  // console.log(storingbox);
-  document.getElementById('graph').innerHTML = storingbox + `<div id="frequency${count}" class="bar"></div>`
+  document.getElementById('graph').innerHTML = storingbox + `<div id="frequency${count}" class="bar"></div>`;
 }
 
 document.getElementById("start-btn").addEventListener("click", startNoiseDetection);
